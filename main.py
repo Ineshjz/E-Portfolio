8c1836eac6933c8cfe93b7d5c6a9c6dc71e8b838
ines <ines.hijazi@epfedu.fr>
2026-04-01 16:50:29 +0200

    [MAIN] Add of requests GET and POST
    Co-authored-by: Jiro-science <Jiro-science@users.noreply.github.com>
    Co-authored-by: MarieBouafou <MarieBouafou@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,32 +1,51 @@
 from fastAPI import FastAPI
 from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
 
 
 app = FastAPI()
 
 
 class PersonalInfo(BaseModel):
-    firtsname : str
-    lastname : str
-    email : str
-    fields : list[str]
-    keywords : list[str]
-    description : str
-    main_skills : list[str]
-    studies : str
-    links : list[str]
+    firstname: str
+    lastname: str
+    email: str
+    fields: list[str]
+    keywords: list[str]
+    description: str
+    main_skills: list[str]
+    studies: str
+    links: list[str]
+
 
 class ProjectDescription(BaseModel):
-    scope : str
-    why : str
-    context : str
-    collaborators : list[str]
-    url : str 
+    scope: str
+    why: str
+    context: str
+    collaborators: list[str]
+    url: str
+
 
 class ProjectVisual(BaseModel):
-    
-    
+    image: str
+    video: str
+
+class Eportfolio(BaseModel):
+    personal_info : PersonalInfo
+    project_description : list[ProjectDescription]
+    projec_visual : list[ProjectVisual]
+
 @app.get("/")
 def home_page():
-    return {"hello"}
+    return
+
+
+@app.get("/eportfolio/{id}")
+def get_portfolio(id:{id}):
+    pass
+
+@app.post("/eportfolio/create")
+def create_portfolio(eportfolio: Eportfolio):
+    return ""
+
 
